34d7243f4475b48cbbbb90a6e84edd22769f7317
Patrick van Zadel <patrick.vanzadel@luminis.eu>
2025-10-31 17:49:57 +0100

    Add Playwright configuration, Avatar component, and Switch component

---
+++ tests/home.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test('should navigate to the home page', async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto('http://localhost:3000/')
+  await expect(page.locator('a[data-slot="breadcrumb-link"]')).toContainText('Home')
+})
